50d17830f21fc831b9a686667cef8383e20a05c2
Danilo Mezgec <danilo@itech.media>
2016-11-20 18:30:50 +0000

    Chai added

---
+++ test/hangman_test.js
@@ -0,0 +1,8 @@
+'use strict';
+var expect = require('chai').expect;
+
+describe('Setup Test', function () {
+    it('Tests if the test suite is working', function () {
+        expect(true).to.be.ok;
+    })
+});
